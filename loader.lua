c3b31ee84d6409ac140bde1230ef2b009fa978e3
0nly6Ex <76264172+0nly6Ex@users.noreply.github.com>
2022-02-05 00:55:01 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,9 @@
+_G.Theme = "Light" --// Choose from "Light" or "Dark" (The default is set to Light)
+_G.Key = "" --// Enter your access key in between the quotes
+
+--[[
+    Key can be found at https://alerion.wtf
+    Please join our discord for more updates at https://discord.gg/K6SRXHMtej
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0nly6Ex/alerion/main/alerion.lua"))()
