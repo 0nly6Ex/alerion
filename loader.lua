612dae3c2c5748442e2f9f453e344f1a190e923a
0nly6Ex <76264172+0nly6Ex@users.noreply.github.com>
2022-02-05 15:12:13 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,21 @@
-_G.Theme = "Light" --// Choose from "Light" or "Dark" (The default is set to Light)
+_G.Theme = "Light" --// Choose the theme you would like to use.
+_G.Style = "Top" --// Choose which menu style you would like to use.
 _G.Key = "" --// Enter your access key in between the quotes
-
+--[[
+    A list of the supported themes are below. Feel free to try out all the themes until you find one you like :)
+     -Dark
+     -Light
+     -Mocha
+     -Aqua
+     -Jester
+]]
+--[[
+    A list of the supported styles are below
+    - Top (Keep all the pages at the top)
+    - Left (Keep all of the pages on the left)
+]]
 --[[
     Key can be found at https://alerion.wtf
     Please join our discord for more updates at https://discord.gg/K6SRXHMtej
 ]]
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/0nly6Ex/alerion/main/alerion.lua"))()
